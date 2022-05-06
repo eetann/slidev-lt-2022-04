4f83fe0871b527a0a790d2d70cc7d788cc985e5d
eetann <hideharu092@gmail.com>
2022-05-06 10:18:56 +0900

    use official example

--- global-bottom.vue
+++ global-bottom.vue
@@ -1,5 +1,8 @@
 <template>
-  <footer class="absolute bottom-0 right-0 p-4">
-    {{ $slidev.nav.currentPage }}
+  <footer
+    v-if="$slidev.nav.currentLayout !== 'cover'"
+    class="absolute bottom-0 left-0 right-0 p-2"
+  >
+    {{ $slidev.nav.currentPage }} / {{ $slidev.nav.total }}
   </footer>
 </template>
