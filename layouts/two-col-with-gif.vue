a75c7c0cc1acb053bdfe4e9aff3ba8d1cd347f5e
eetann <hideharu092@gmail.com>
2022-05-06 10:18:30 +0900

    add comment

--- layouts/two-col-with-gif.vue
+++ layouts/two-col-with-gif.vue
@@ -11,15 +11,18 @@ const props = withDefaults(defineProps<Props>(), {
   <div class="slidev-layout">
     <!-- 縦に要素を並べる -->
     <div class="h-full flex flex-col">
+      <!-- 1. 見出し -->
       <slot name="title" />
 
-      <!-- 2列 -->
+      <!-- 2. 「黄色の背景に文字」+「文字」の組み合わせが左右に並んだもの -->
       <div class="grid grid-cols-2">
+        <!-- ショートカットキー 1 -->
         <MyKbd keybind="j" height="12" textSize="text-3xl">
           ：
           <slot name="j_feature" />
         </MyKbd>
 
+        <!-- ショートカットキー 2 -->
         <MyKbd keybind="k" height="12" textSize="text-3xl">
           ：
           <slot name="k_feature" />
@@ -28,17 +31,22 @@ const props = withDefaults(defineProps<Props>(), {
 
       <!-- 下寄せ かつ 左右の端に要素を並べる -->
       <div class="mt-auto flex justify-between">
+        <!-- 3. GIF -->
         <slot />
 
         <!-- 縦に要素を並べる -->
         <div class="flex flex-col">
           <!-- 上下中央 -->
           <div class="my-auto">
+            <!-- 4. 説明欄 -->
+            <!-- ショートカットキー 3 -->
             <slot name="description" />
           </div>
 
-          <!-- 横に要素を並べる -->
+          <!-- 5. ショートカットキーのヘルプ -->
+          <!-- 背景緑で中央に配置 -->
           <div class="flex items-center bg-lime-300 h-12 p-4">
+            <!-- ショートカットキー 4 -->
             <MyKbd
               v-bind:keybind="props.help_key"
               height="8"
